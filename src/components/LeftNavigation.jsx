f6f82daa5ba5c741a21493e9e721e3cbe2a5d559
Christian Noel Reyes <darkcolonist@gmail.com>
2023-07-18 08:48:01 +0000

    Update LeftNavigation.jsx and MainLayout.jsx simplified and centralized navigation into LeftNavigation to reduce code repetitiveness and promote brevity

--- src/components/LeftNavigation.jsx
+++ src/components/LeftNavigation.jsx
@@ -1,19 +1,50 @@
 import {
   ListItemIcon,
   MenuItem as DefaultMenuItem,
-  MenuList
+  MenuList,
 } from "@mui/material";
 import { NavLink } from "react-router-dom";
 import HomeIcon from "@mui/icons-material/Home";
 import AnimationIcon from "@mui/icons-material/Animation";
 import BookmarkIcon from "@mui/icons-material/Bookmark";
+import WelcomePage from "./WelcomePage";
+import TransitionTest from "./TransitionTest";
+import Demo1Page from "./Demo1Page";
+import ConfirmButtonPage from "./ConfirmButtonPage";
+
+export const sections = [
+  {
+    path: "/",
+    name: "Home",
+    icon: HomeIcon,
+    component: WelcomePage,
+  },
+  {
+    path: "/transitions",
+    name: "Transitions",
+    icon: AnimationIcon,
+    component: TransitionTest,
+  },
+  {
+    path: "/demo1",
+    name: "Demo 1",
+    icon: BookmarkIcon,
+    component: Demo1Page,
+  },
+  {
+    path: "/confirm-button-with-cooldown",
+    name: "Confirm Button with Cooldown",
+    icon: BookmarkIcon,
+    component: ConfirmButtonPage,
+  },
+];
 
 const MenuItem = (props) => (
   <DefaultMenuItem
     {...props}
     style={{
       whiteSpace: "normal",
-      minWidth: "140px"
+      minWidth: "140px",
     }}
   />
 );
@@ -22,39 +53,19 @@ export default function () {
   const navProps = {
     className: (isActive) => {
       console.log("found", isActive);
-    }
+    },
   };
 
   return (
     <MenuList>
-      <MenuItem component={NavLink} to="/" {...navProps}>
-        <ListItemIcon>
-          <HomeIcon />
-        </ListItemIcon>
-        Home
-      </MenuItem>
-      <MenuItem component={NavLink} to="transitions" {...navProps}>
-        <ListItemIcon>
-          <AnimationIcon />
-        </ListItemIcon>
-        Transitions
-      </MenuItem>
-      <MenuItem component={NavLink} to="demo1" {...navProps}>
-        <ListItemIcon>
-          <BookmarkIcon />
-        </ListItemIcon>
-        Demo 1
-      </MenuItem>
-      <MenuItem
-        component={NavLink}
-        to="confirm-button-with-cooldown"
-        {...navProps}
-      >
-        <ListItemIcon>
-          <BookmarkIcon />
-        </ListItemIcon>
-        Confirm Button with Cooldown
-      </MenuItem>
+      {sections.map((el) => (
+        <MenuItem component={NavLink} to={el.path} {...navProps} key={el.name}>
+          <ListItemIcon>
+            <el.icon />
+          </ListItemIcon>
+          {el.name}
+        </MenuItem>
+      ))}
     </MenuList>
   );
 }
--- src/components/MainLayout.jsx
+++ src/components/MainLayout.jsx
@@ -1,11 +1,7 @@
 import { Grid, Typography } from "@mui/material";
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import ConfirmButtonPage from "./ConfirmButtonPage";
-import Demo1Page from "./Demo1Page";
-import LeftNavigation from "./LeftNavigation";
-import TransitionTest from "./TransitionTest";
-import WelcomePage from "./WelcomePage";
+import LeftNavigation, { sections } from "./LeftNavigation";
 
 export default function () {
   return (
@@ -20,13 +16,9 @@ export default function () {
           </Grid>
           <Grid item xs={10}>
             <Routes>
-              <Route path="/" element={<WelcomePage />} />
-              <Route path="transitions" element={<TransitionTest />} />
-              <Route path="demo1" element={<Demo1Page />} />
-              <Route
-                path="confirm-button-with-cooldown"
-                element={<ConfirmButtonPage />}
-              />
+              {sections.map((el) => (
+                <Route path={el.path} key={el.name} element=<el.component /> />
+              ))}
             </Routes>
           </Grid>
         </Grid>
