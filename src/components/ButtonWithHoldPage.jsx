68ece383fe28f3a12bac00d3de965d127b432fe3
Christian Noel Reyes <darkcolonist@gmail.com>
2023-07-19 02:44:51 +0000

    Update ButtonWithHoldPage.jsx and HoldButtonIcon.jsx added done icon if doneicon is specified, succeeding actions will no longer trigger

--- src/components/ButtonWithHoldPage.jsx
+++ src/components/ButtonWithHoldPage.jsx
@@ -2,12 +2,13 @@ import React from "react";
 import DeleteForeverIcon from "@mui/icons-material/DeleteForever";
 import HoldButtonIcon from "./HoldButtonIcon";
 import { Typography } from "@mui/material";
+import DoneIcon from "@mui/icons-material/Done";
 
 export default function () {
   const [message, setMessage] = React.useState("ready");
 
   const handleHeavyAction = () => {
-    setMessage("deleted");
+    setMessage((currentMessage) => currentMessage + " " + "deleted");
   };
   return (
     <React.Fragment>
@@ -15,6 +16,7 @@ export default function () {
       <HoldButtonIcon
         aria-label="heavy action"
         color="error"
+        doneicon={<DoneIcon color="success" />}
         onClick={handleHeavyAction}
         timeout={2000}
       >
--- src/components/HoldButtonIcon.jsx
+++ src/components/HoldButtonIcon.jsx
@@ -6,6 +6,7 @@ export default (props) => {
   const holdTimerRef = useRef(null);
   const pressedRef = useRef(false);
   const [isPressed, setIsPressed] = React.useState(false);
+  const [extendedProps, setExtendedProps] = React.useState({});
 
   let currentTimeout = props.timeout ?? defaultTimeout;
 
@@ -15,6 +16,23 @@ export default (props) => {
     holdTimerRef.current = setTimeout(() => {
       if (pressedRef.current) {
         // alert("Process done");
+        setExtendedProps((currentExtendedProps) => {
+          let extendedPropListeners = {};
+
+          if (props.doneicon) {
+            extendedPropListeners = {
+              onMouseDown: null,
+              onMouseUp: null,
+              onMouseLeave: null,
+            };
+          }
+
+          return {
+            ...currentExtendedProps,
+            ...extendedPropListeners,
+            children: props.doneicon ?? props.children,
+          };
+        });
         props.onClick();
       }
     }, currentTimeout);
@@ -26,39 +44,58 @@ export default (props) => {
     clearTimeout(holdTimerRef.current);
   }
 
+  const initialExtendedProps = {
+    onMouseDown: handleMouseDown,
+    onMouseUp: handleMouseUp,
+    onMouseLeave: handleMouseUp,
+  };
+
+  const styleProps = {
+    className: isPressed ? "clickAndHoldAnimate" : "",
+    sx: {
+      "&.clickAndHoldAnimate": {
+        animation: `clickAndHoldAnimation ${currentTimeout / 1000}s`,
+      },
+      "@keyframes clickAndHoldAnimation": {
+        // shake animation
+        // "0%": { transform: "translateX(0)" },
+        // "10%": { transform: "translateX(-3px)" },
+        // "20%": { transform: "translateX(3px)" },
+        // "30%": { transform: "translateX(-3px)" },
+        // "40%": { transform: "translateX(3px)" },
+        // "50%": { transform: "translateX(-3px)" },
+        // "60%": { transform: "translateX(3px)" },
+        // "70%": { transform: "translateX(-3px)" },
+        // "80%": { transform: "translateX(3px)" },
+        // "90%": { transform: "translateX(-3px)" },
+        // "100%": { transform: "translateX(0)" },
+
+        "0%": { transform: "scale(1)" },
+        "10%": { transform: "scale(1.2)" },
+        "20%": { transform: "scale(1.3)" },
+        "30%": { transform: "scale(1.4)" },
+        "40%": { transform: "scale(1.5)" },
+        "50%": { transform: "scale(1.6)" },
+        "60%": { transform: "scale(1.7)" },
+        "70%": { transform: "scale(1.8)" },
+        "80%": { transform: "scale(1.9)" },
+        "90%": { transform: "scale(2)" },
+        "100%": { transform: "scale(0)" },
+      },
+    },
+  };
+
+  React.useEffect(() => {
+    setExtendedProps(initialExtendedProps);
+  }, []);
+
   return (
     <React.Fragment>
       <IconButton
         {...props}
         onClick={() => {}}
-        onMouseDown={handleMouseDown}
-        onMouseUp={handleMouseUp}
-        onMouseLeave={handleMouseUp}
-        sx={{
-          "&.extended-click": {
-            animation: "extendedClickAnimation 5s",
-          },
-          "&.shake": {
-            animation: "shakeAnimation 0.8s infinite",
-          },
-          "@keyframes extendedClickAnimation": {
-            /* Define your extended click animation keyframes here */
-          },
-          "@keyframes shakeAnimation": {
-            "0%": { transform: "translateX(0)" },
-            "10%": { transform: "translateX(-5px)" },
-            "20%": { transform: "translateX(5px)" },
-            "30%": { transform: "translateX(-6px)" },
-            "40%": { transform: "translateX(6px)" },
-            "50%": { transform: "translateX(-7px)" },
-            "60%": { transform: "translateX(7px)" },
-            "70%": { transform: "translateX(-8px)" },
-            "80%": { transform: "translateX(8px)" },
-            "90%": { transform: "translateX(-9px)" },
-            "100%": { transform: "translateX(0)" },
-          },
-        }}
-        className={isPressed ? "extended-click shake" : ""}
+        {...extendedProps}
+        {...styleProps}
       />
     </React.Fragment>
   );
